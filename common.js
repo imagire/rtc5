2153797286f1e6f669d7714a002bf2eac569c161
imagire <imagire@gmail.com>
2017-09-10 07:46:58 +0000

    deploy commit from travis

---
+++ common.js
@@ -0,0 +1,4 @@
+var html = '';
+html += '更新時刻:Sun Sep 10 07:46:52 UTC 2017<br>';
+html += 'frame count:258<br>memory alloc[sec]:0.197567<br>load IBL[sec]:1.81728<br>1st_render[sec]:3.40729<br>median[sec]:2.34797<br>luminance[sec]:0.87452<br>edge[sec]:0.730051<br>edge_blurred[sec]:5.30029<br>normal[sec]:0.680578<br><br>';
+document.getElementById('include').innerHTML = html;
